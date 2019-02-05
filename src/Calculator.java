34486a28b007e68e1f525c55fa5c2e4d36572fff
taniakolesnik <kolesnik@gmail.com>
2019-02-05 17:26:30 +0000

    add listeners to buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener {
 
+    private JButton buttonOne;
     private JButton buttonTwo;
-    private JButton buttonFive;
     private JButton buttonThree;
+    private JButton buttonFour;
+    private JButton buttonFive;
     private JButton buttonSix;
+    private JButton buttonSeven;
     private JButton buttonEight;
     private JButton buttonNine;
-    private JButton buttonFour;
-    private JButton buttonOne;
-    private JButton buttonSeven;
-    private JButton buttonDelete;
     private JButton buttonZero;
+
+    private JButton buttonDelete;
     private JButton buttonDelimeter;
     private JPanel displayPanel;
     private JTextField displayTextField;
@@ -23,6 +27,23 @@ public class Calculator extends JFrame {
         setTitle("Calculator");
         setSize(400, 300);
         add(rootPanel);
-        System.out.println("T   ");
+
+        setButtonListeners();
+
+    }
+
+    private void setButtonListeners() {
+        for (Component component : rootPanel.getComponents()){
+            if (component.getClass() == JButton.class){
+                ((JButton) component).addActionListener(this);
+            }
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+            Object source = e.getSource();
+            if (source instanceof JButton) System.out.println(e.getActionCommand());
     }
 }
